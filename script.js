96848da1c564640cb08bfcb8006bad67924d8528
Sobiwa <kevin.b.collister@gmail.com>
2022-09-11 15:40:38 -0400

    Make a complete mess

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function getComputerChoice() {
     }
 }
 
+
 function remove() {
     option.classList.remove('clicked');
 }
@@ -36,24 +37,25 @@ const scoreContainer = document.querySelector('#score');
 
 function createScoreboard(){    
     const userScoreDisplay = document.createElement('div');
-    userScoreDisplay.style.cssText = 'display:flex; flex-direction: column; gap: 8px';
+    userScoreDisplay.style.cssText = 'display:flex; gap: 8px';
     scoreContainer.appendChild(userScoreDisplay);
         const userScoreString = document.createElement('p');
         userScoreString.textContent = 'Your Score: ';
-        const userScoreValue = document.createElement('p');
-        userScoreValue.textContent = `${userScore}`;
+        let userScoreValue = document.createElement('p');
+        userScoreValue.textContent =0 ; 
         userScoreDisplay.appendChild(userScoreString);
         userScoreDisplay.appendChild(userScoreValue);
+        userScoreValue.textContent = 1;
         
     const compScoreDisplay = document.createElement('div');
     compScoreDisplay.style.cssText = 'display:flex; gap: 8px';
     scoreContainer.appendChild(compScoreDisplay);
         const compScoreString = document.createElement('p');
         compScoreString.textContent = 'My Score: ';
-        const compScoreValue = document.createElement('p');
-        compScoreValue.textContent = `${compScore}`;
-        compScoreDisplay.appendChild(userScoreString);
-        compScoreDisplay.appendChild(userScoreValue);
+        let compScoreValue = document.createElement('p');
+        compScoreValue.textContent = 0;
+        compScoreDisplay.appendChild(compScoreString);
+        compScoreDisplay.appendChild(compScoreValue);
 };
 
 const yesButton = document.createElement('button');
@@ -96,9 +98,9 @@ yesButton.addEventListener('click', () => {
     beginPromptContainer.removeChild(beginPrompt);
     beginPromptContainer.removeChild(yesButton);
     rpsCountdown();
-
-
+    game();
 });
+
 
 // psuedo code 
 // player clicks yes
@@ -108,16 +110,35 @@ yesButton.addEventListener('click', () => {
 // plays round
 // first player to 5 wins
 
+const ROCK = document.querySelector('#rock');
+const PAPER = document.querySelector('#paper');
+const SCISSORS = document.querySelector('#scissors');
+
+
+
+
 
 const options = document.querySelectorAll('.option');
 console.log(options);
 options.forEach ((option) => {
-    option.addEventListener('click', () => {
-        console.log(option.id);
+        option.addEventListener('click', () => {
         option.classList.add('clicked');
         setTimeout(() => option.classList.remove('clicked'),150);
     });
 });
+
+// function getCHOICE() {
+//     options.forEach (option) => {
+//         option.addEventListener('click', function getUserChoice(option.id) {
+//             if option.id == 'rock'
+//         });
+//     };
+// };
+// function getUserChoice(optionID) {
+//     if (optionID == 'rock') return 'rock';
+//     else if (optionID == 'paper') return 'paper';
+//     else if (optionID == 'scissors') return 'scissors';
+// };
 
 function playRound(userPick, compPick) {
     if (userPick === null) {
@@ -125,7 +146,7 @@ function playRound(userPick, compPick) {
         return 'null';
     }
     else {
-        userPick = userPick.toLowerCase();
+        // userPick = userPick.toLowerCase();
         if (compPick === userPick) {
             return 'tie';
         }
@@ -151,50 +172,110 @@ function playRound(userPick, compPick) {
             return 'empty';
         }
     }
-}
+};
 
-let userPick = 'rock';
+function addPoint(result) {
+    if (result === 'win') {
+        userScore += 1;
+        userScoreValue.textContent += 1;
+    }
+    else if (result === 'lose') {
+        compScore += 1;
+        compScoreValue.textContent += 1;
+    }
+    else if (result === 'tie') {
+    }
+};
+
 function game() {
+    createScoreboard();
     let compScore = 0;
     let userScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let userPick = prompt('Rock, paper, scissors, and shoot!', '');
-        compPick = getComputerChoice();
-        let result = playRound(userPick,compPick);
-        console.log(result);
-        if (result === 'win') {
-            userScore += 1;
-            alert(`You played ${userPick}, I played ${compPick}. You win. Let's go ${4-i} more`);
-        }
-        else if (result === 'lose') {
-            compScore += 1;
-            alert(`You played ${userPick}, I played ${compPick}. You lose. Let's go ${4-i} more`);
-        }
-        else if (result === 'tie') {
-            alert('tie!');
-        }
-        else if (result == 'null') {
-            return
-        }
-        else {
-            i = --i;
-            alert('Play fair!');
-        }
-        alert(`Computer score: ${compScore} \nUser score: ${userScore}`)
-        console.log(`Computer played ${compPick} \nUser played ${userPick}`);
-        console.log(`Computer score: ${compScore} \nUser score: ${userScore}`);
-        console.log('next round...');
-    }
-    if (compScore > userScore) {
-        alert(`Not your day! My score: ${compScore} Your score: ${userScore}`);
-        return `Not your day! My score: ${compScore} Your score: ${userScore}`;
-    }
-    else if (compScore <userScore) {
-        alert(`You got lucky... My score: ${compScore} Your score: ${userScore}`);
-        return `You got lucky... My score: ${compScore} Your score: ${userScore}`;
-    }
-    else {
-        alert(`It's a tie! What a wast of time. My score: ${compScore} Your score: ${userScore}`);
-        return `It's a tie! What a wast of time. My score: ${compScore} Your score: ${userScore}`;
-    }  
-}
+    // while (compScore < 5 && userScore < 5) {
+        ROCK.addEventListener('click', () => {
+            let result = playRound('rock', getComputerChoice());
+            if (result === 'win') {
+                userScore += 1;
+                userScoreDisplay.removeChild(userScoreValue);
+                const userScoreValue = document.createElement(p);
+                userScoreValue.textContent = `${userScore}`;
+                userScoreDisplay.appendChild(userScoreValue);
+            }
+            else if (result === 'lose') {
+                compScore += 1;
+                userScore += 1;
+                userScoreDisplay.removeChild(userScoreValue);
+                const userScoreValue = document.createElement(p);
+                userScoreValue.textContent = `${userScore}`;
+                userScoreDisplay.appendChild(userScoreValue);
+            }
+            else if (result === 'tie') {
+            }
+        });
+        PAPER.addEventListener('click', () => {
+            let result = playRound('paper', getComputerChoice());
+            if (result === 'win') {
+                userScore += 1;
+                userScoreValue.textContent += 1;
+            }
+            else if (result === 'lose') {
+                compScore += 1;
+                compScoreValue.textContent += 1;
+            }
+            else if (result === 'tie') {
+            }
+        });
+        SCISSORS.addEventListener('click', () => {
+                let result = playRound('scissors', getComputerChoice());
+                if (result === 'win') {
+                    userScore += 1;
+                    userScoreValue.textContent += 1;
+                }
+                else if (result === 'lose') {
+                    compScore += 1;
+                    compScoreValue.textContent += 1;
+                }
+                else if (result === 'tie') {
+                }
+        });
+};
+
+        // let userPick = getUserChoice();
+        // let compPick = getComputerChoice();
+        // let result = playRound(userPick,compPick);
+        // console.log(result);
+        // if (result === 'win') {
+        //     userScore += 1;
+            // alert(`You played ${userPick}, I played ${compPick}. You win. Let's go ${4-i} more`);
+        // }
+        // else if (result === 'lose') {
+        //     compScore += 1;
+            // alert(`You played ${userPick}, I played ${compPick}. You lose. Let's go ${4-i} more`);
+        // }
+        // else if (result === 'tie') {
+            // alert('tie!');
+        // }
+        // else if (result == 'null') {
+        //     return
+        // }
+        // else {
+        //     return;
+        // }
+        // alert(`Computer score: ${compScore} \nUser score: ${userScore}`)
+//         console.log(`Computer played ${compPick} \nUser played ${userPick}`);
+//         console.log(`Computer score: ${compScore} \nUser score: ${userScore}`);
+//         console.log('next round...');
+//     }
+//     if (compScore > userScore) {
+//         alert(`Not your day! My score: ${compScore} Your score: ${userScore}`);
+//         return `Not your day! My score: ${compScore} Your score: ${userScore}`;
+//     }
+//     else if (compScore <userScore) {
+//         alert(`You got lucky... My score: ${compScore} Your score: ${userScore}`);
+//         return `You got lucky... My score: ${compScore} Your score: ${userScore}`;
+//     }
+//     else {
+//         alert(`It's a tie! What a wast of time. My score: ${compScore} Your score: ${userScore}`);
+//         return `It's a tie! What a wast of time. My score: ${compScore} Your score: ${userScore}`;
+//     }  
+// }
